40faa8b8bcbe925ad7860ccd01ec7eb011fcc6f4
Pekhenka1 <137805390+Pekhenka1@users.noreply.github.com>
2025-09-29 23:12:50 +0300

    Update merkleProof.js

--- frontend/functions/merkleProof.js
+++ frontend/functions/merkleProof.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch')
 
 const AUTH = process.env.NFTPORT_API_KEY;
-const include = "merkle_proofs";
 
 exports.handler = async (event, context) => {
   const wallet = event.queryStringParameters && event.queryStringParameters.wallet
